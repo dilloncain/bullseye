a53ff68fe7d2de3c8a4f87b6036e57b0cd831099
Dillon Cain <dilloncain@caincomputer.com>
2019-03-12 21:30:05 -0700

    Added About Author VC

---
+++ BullsEye/BullsEye/AuthorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AuthorViewController.swift
+//  BullsEye
+//
+//  Created by Dillon Cain on 3/12/19.
+//  Copyright © 2019 Cain Computers. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
